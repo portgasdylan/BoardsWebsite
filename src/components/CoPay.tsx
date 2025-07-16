eb15a4c8a0f4d081d13cfffcd474604345ce10c8
Dylan Maxwell <113061279+portgasdylan@users.noreply.github.com>
2025-07-15 21:18:02 -0700

    Updated CoPay.tsx

--- src/components/CoPay.tsx
+++ src/components/CoPay.tsx
@@ -76,6 +76,7 @@ const CoPay: React.FC = () => {
                   >
                     <ExternalLink className="w-4 h-4" />
                     <span>Live Demo</span>
+                    />
                   </button>
                 </div>
               </div>
